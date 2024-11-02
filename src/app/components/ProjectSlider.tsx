55a2d9174773ae9e23f3c2fda004f990bc54bcbd
omo-Akeye <akeyesaheed@gmail.com>
2024-11-02 10:00:50 +0100

    implement mobile responsiveness

--- src/app/components/ProjectSlider.tsx
+++ src/app/components/ProjectSlider.tsx
@@ -1,31 +1,44 @@
+
 import React, { useState } from 'react';
 
 type Project = {
   title: string;
   description: string;
   hoverIMG: string;
+  Link: string;
+  year:number;
+  descriptionLink?: string;
 };
 
 const projects: Project[] = [
   {
     title: "Bime website revamp",
-    description: "2024 - UI design, UX improvement, development",
+    description: "UI design, UX improvement, development.Copywriting",
     hoverIMG: "/Group 11.svg",
+    Link: "https://revamp-xi.vercel.app/",
+    year:2024,
+    descriptionLink:"https://x.com/copybyhabeeb"
   },
   {
     title: "Bime website revamp",
-    description: "2024 - UI design, UX improvement, development",
+    description: "UI design, UX improvement, development",
     hoverIMG: "/Group 11.svg",
+    Link: "https://revamp-xi.vercel.app/",
+    year:2024
   },
   {
     title: "Bime website revamp",
-    description: "2024 - UI design, UX improvement, development",
+    description: "UI design, UX improvement, development",
     hoverIMG: "/Group 11.svg",
+    Link: "https://revamp-xi.vercel.app/",
+    year:2024
   },
   {
     title: "Bime website revamp",
-    description: "2024 - UI design, UX improvement, development",
+    description: "UI design, UX improvement, development",
     hoverIMG: "/Group 11.svg",
+    Link: "https://revamp-xi.vercel.app/",
+    year:2024
   },
 
 ];
@@ -34,22 +47,42 @@ export default function ProjectSlider() {
   const [hoveredProjectIndex, setHoveredProjectIndex] = useState<number | null>(null);
 
   return (
-    <div className="inline-flex flex-col space-y-4">
+    <div className="inline-flex flex-col space-y-4 xl:w-[45%] sm:w-[55%] w-full">
       {projects.map((project, index) => (
         <div
           key={index}
-          className="px-5 py-4 rounded-[10px] hover:bg-[#000000] hover:bg-opacity-10 inline-block cursor-pointer hover:ease-in-out"
+         
           onMouseEnter={() => setHoveredProjectIndex(index)}
           onMouseLeave={() => setHoveredProjectIndex(null)}
         >
-          <h1 className="underline capitalize mb-[8px]">{project.title}</h1>
-          <p className="text-[#000000] text-opacity-60">{project.description}</p>
+          <div  className="px-5 py-4 rounded-[10px] hover:bg-[#000000] hover:bg-opacity-10 inline-flex flex-col cursor-pointer hover:ease-in-out max-sm:relative">
+          <a href={`${project.Link}`} className="underline capitalize mb-[8px] inline-flex">{project.title}</a>
+        
+             
+          
+            <div className='flex items-center text-[#000000] text-opacity-60 text-sm'>
+            <p >
+            {project.year} 
+            </p>
+           <p className='mx-2'>-</p>
+            <p> 
+            {project.description}
+                {project.descriptionLink && (
+                  <a href={project.descriptionLink} className=" underline" target="_blank" rel="noopener noreferrer">
+                    (by copybyhabeeb)
+                  </a>
+                )}
+            </p>
+            </div>
+         
+          </div>
+         
           {hoveredProjectIndex === index && (
-            <div className="absolute flex items-center justify-center rounded-[10px] transition-opacity duration-300 ease-in-out max-w-[550px] max-h-80 right-0 top-[10%]">
+            <div className="absolute flex items-center justify-center rounded-[10px] transition-opacity duration-300 ease-in-out max-w-[550px] max-h-80 sm:right-0 sm:top-[10%] max-md:w-[300px] z-20 max-sm:w-[200px] max-sm:right-0">
               <img
                 src={project.hoverIMG}
                 alt="Project preview"
-                className="w-full h-full object-cover"
+                className="lg:w-full lg:h-full object-cover md:w-[80%] md:h-[80%] h-full w-full"
               />
             </div>
           )}
@@ -57,4 +90,8 @@ export default function ProjectSlider() {
       ))}
     </div>
   );
-}
+} 
+
+
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
 
   return (
     <div className="max-w-[1440px] mx-auto  orbit-regular mb-32">
-      <nav className="w-[90%] flex justify-between mt-[3%] mx-auto items-center relative">
-        <span className="flex items-center gap-4">
+      <nav className="w-[90%] flex max-md:flex-col justify-between md:mt-[3%] mt-[41px] mx-auto items-center relative">
+        <span className="flex items-center gap-4 relative">
           <img src="/Group 2.svg" alt="profile" width={60} className="cursor-pointer"/>
           <div className="flex items-center gap-x-2 px-3 py-2 border-[#D9D9D9] border-2 rounded-[36.88px]">
             <img src="/Ellipse 4.svg" alt="active" width={14} />
-            <p className="text-xs">
+            <p className="md:text-xs text-[10px]">
               <span className="mr-2 text-[#000000] text-opacity-60">Status:</span> Open to work
             </p>
           </div>
@@ -28,7 +28,7 @@ export default function Home() {
           >
            <img src="/Frame 418.svg" alt="" className=""/>  
             {showChatPopup && (
-              <div className="rounded-md transition-opacity duration-300  bg-[#F5F5F5] flex items-center ml-2 py-2 px-1">
+              <div className="rounded-md transition-opacity duration-300  bg-[#F5F5F5] flex items-center ml-2 py-2 px-1 max-md:absolute max-md:right-[4%] max-md:bottom-[-55%]">
                 Let’s Chat
               </div>
             )}
@@ -41,7 +41,7 @@ export default function Home() {
 
 
 
-        <div className="flex gap-x-6">
+        <div className="md:flex gap-x-6 hidden">
           <p className="flex underline items-center gap-x-3">
             Links <img src="/Vector (Stroke).svg" alt="arrow" />
           </p>
@@ -63,16 +63,16 @@ export default function Home() {
             {showAboutPopup && (
               <div className="absolute top-8 right-0 rounded-md transition-opacity duration-300 ease-in-out opacity-100 mt-8 max-w-[350px]">
                 <div className="bg-[#F5F5F5]  flex py-[10px] px-6 items-center rounded-lg cursor-pointer hover:text-gray-500"> <img src="/abstract.svg" width={34} className="mr-[10px]"/>Our micro team</div>
-                <ul className="mt-3 space-y-3">
-                  <li className="flex items-center bg-[#F5F5F5]  py-[10px] px-6 flex-shrink-0 rounded-lg cursor-pointer hover:text-gray-500">
+                <div className="mt-3 space-y-3">
+                  <a href="https://www.behance.net/isaacoreoluwa" className="flex items-center bg-[#F5F5F5]  py-[10px] px-6 flex-shrink-0 rounded-lg cursor-pointer hover:text-gray-500">
                     <img src="/ore.svg" alt="Isaac Oreoluwa" width={34} className="mr-[10px]" />
                     <span>Isaac Oreoluwa - Designer</span>
-                  </li>
-                  <li className="flex items-center bg-[#F5F5F5] py-[10px] px-6 flex-shrink-0 rounded-lg cursor-pointer hover:text-gray-500">
+                  </a>
+                  <a href="https://akeye-portfolio.netlify.app/" className="flex items-center bg-[#F5F5F5] py-[10px] px-6 flex-shrink-0 rounded-lg cursor-pointer hover:text-gray-500">
                     <img src="/akeye.svg" alt="Akeye Saheed" width={34} className="mr-[10px]" />
                     <span>Akeye Saheed - Developer</span>
-                  </li>
-                </ul>
+                  </a>
+                </div>
               </div>
             )}
 
@@ -82,14 +82,54 @@ export default function Home() {
         </div>
       </nav>
 
-      <section className="flex items-center w-[40%] mx-auto gap-x-3 mt-24">
-        <p>Design <span className="text-[#000000] text-opacity-60 underline">Isaac Oreoluwa</span></p>
-        <span>.</span>
-        <p>Development <span className="text-[#000000] text-opacity-60 underline">Akeye Saheed</span></p>
+      <section className="flex md:items-center md:justify-center md:w-[80%] w-[90%] xl:w-[50%] md:mt-24 mx-auto mt-[68px] max-md:flex-col ">
+        <div className="flex gap-x-3"><img src="/Layers.svg" alt="design" />Design <a href="https://www.behance.net/isaacoreoluwa" className="text-[#000000] text-opacity-60 underline">Isaac Oreoluwa</a></div>
+        <img src="/dot.png" alt="dot" className="mx-6 max-md:hidden" />
+        <div className="flex gap-x-3 max-md:mt-2"> <img src="/Code 2.svg" alt="code"  />Development <a href="https://akeye-portfolio.netlify.app/" className="text-[#000000] text-opacity-60 underline">Akeye Saheed</a></div>
       </section>
 
-      <section className="mt-[70px] relative w-[80%] mx-auto">
-        <h2 className="uppercase ml-5 mb-6">Featured Projects</h2>
+
+      <div className="flex gap-x-6 md:hidden mt-10 w-[90%] mx-auto relative ">
+          <p className="flex underline items-center gap-x-3">
+            Links <img src="/Vector (Stroke).svg" alt="arrow" />
+          </p>
+          
+
+          <div >
+          <p
+              className="flex underline items-center gap-x-3 cursor-pointer"
+              onClick={() => setShowAboutPopup(!showAboutPopup)}
+            >
+              About{" "}
+              <img
+                src="/Vector (Stroke).svg"
+                alt="arrow"
+                className={`transition-transform duration-300 ${showAboutPopup ? "rotate-180" : ""}`}
+              />
+            </p>
+       
+            {showAboutPopup && (
+              <div className="absolute top-0 left-2 rounded-md transition-opacity duration-300 ease-in-out opacity-100 mt-8 max-w-[350px] z-20 sm:bg-white">
+                <div className="bg-[#F5F5F5]  flex py-[10px] px-6 items-center rounded-lg cursor-pointer hover:text-gray-500"> <img src="/abstract.svg" width={34} className="mr-[10px]"/>Our micro team</div>
+                <div className="mt-3 space-y-3">
+                  <a href="https://www.behance.net/isaacoreoluwa" className="flex items-center bg-[#F5F5F5]  py-[10px] px-6 flex-shrink-0 rounded-lg cursor-pointer hover:text-gray-500">
+                    <img src="/ore.svg" alt="Isaac Oreoluwa" width={34} className="mr-[10px]" />
+                    <span>Isaac Oreoluwa - Designer</span>
+                  </a>
+                  <a href="https://akeye-portfolio.netlify.app/" className="flex items-center bg-[#F5F5F5] py-[10px] px-6 flex-shrink-0 rounded-lg cursor-pointer hover:text-gray-500">
+                    <img src="/akeye.svg" alt="Akeye Saheed" width={34} className="mr-[10px]" />
+                    <span>Akeye Saheed - Developer</span>
+                  </a>
+                </div>
+              </div>
+            )}
+
+          </div>
+        
+        </div>
+
+      <section className="md:mt-[70px] relative lg:w-[80%] mx-auto mt-14 w-[90%]">
+        <h2 className="uppercase md:ml-5 mb-6">Featured Projects</h2>
       
 
      <ProjectSlider/>
